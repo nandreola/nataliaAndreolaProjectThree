79c8acea6522a6f2dec1af9b94dfe9f82449f346
Natalia <nathy.andreola@gmail.com>
2019-08-20 23:23:59 -0400

    setup js file and added array of questions

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// Set up the array of questions and answers
+const quiz = [
+    {
+        question: 'q1', 
+        answers: [
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't1',
+                score: 1
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't2',
+                score: 0
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't3',
+                score: -1
+            }
+        ],
+        background: 'bg-red',
+        type: 'adult' 
+    },
+    {
+        question: 'q2',
+        answers: [
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't1',
+                score: 1
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't2',
+                score: 0
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't3',
+                score: -1
+            }
+        ],
+        background: 'bg-green',
+        type: 'adult'
+    },
+    {
+        question: 'q3',
+        answers: [
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't1',
+                score: 1
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't2',
+                score: 0
+            },
+            {
+                image: 'assets/imagepath.jpg',
+                text: 't3',
+                score: -1
+            }
+        ],
+        background: 'bg-red',
+        type: 'adult'
+    }
+];
+
+
+
+
+
